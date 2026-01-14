43e4093202988f5c54882f621dd8cce3160a99ff
AmiChanDev <gamageamantha@gmail.com>
2026-01-14 22:23:13 +0530

    Initial Commit

---
+++ app/src/main/java/activity/HomeActivity.java
@@ -0,0 +1,21 @@
+package activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//Layout file configuration and initialization
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
