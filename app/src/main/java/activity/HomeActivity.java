40c9d5e6d9f2ac1a9c8cb9815219b532e20470b2
AmiChanDev <gamageamantha@gmail.com>
2026-01-14 23:13:39 +0530

    commit changes

--- app/src/main/java/activity/HomeActivity.java
+++ app/src/main/java/activity/HomeActivity.java
@@ -1,15 +1,22 @@
 package activity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import lk.jiat.intent.application.R;
+
 public class HomeActivity extends AppCompatActivity {
+    private Button homeBtn;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //Layout file configuration and initialization
+        setContentView(R.layout.activity_home);
+        this.homeBtn = findViewById(R.id.homeBtn);
 
 
     }
